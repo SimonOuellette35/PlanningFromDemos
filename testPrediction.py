141981b06ed3501c1457523a36d68717096be981
SimonOuellette35 <chiefscientist@outlook.com>
2022-07-02 16:53:36 -0400

    testPrediction now also shows uncertainties

--- testPrediction.py
+++ testPrediction.py
@@ -1,7 +1,7 @@
 from gym_minigrid.wrappers import *
 import torch
 import utils
-from model.ConvictionPlanner import MotionPlanner
+from model.ConvictionPlanner import ConvictionPlanner
 import numpy as np
 import matplotlib.pyplot as plt
 import pickle
@@ -11,6 +11,7 @@ ACTION_SPACE = 3
 Z_DIM = 512
 NC = 3
 LOAD_DND = True
+CALC_MAE = False
 
 model_path = './saved_models/best_full_modelV3'
 data_dir = 'BC_baseline/data/'
@@ -28,7 +29,7 @@ elif GRID_DIFFICULTY == 'Intermediate':
     TILE_SIZE = 12
 
 # load MotionPlanner model
-model = MotionPlanner(action_space=ACTION_SPACE, device=device, dict_len=25000)
+model = ConvictionPlanner(action_space=ACTION_SPACE, device=device, dict_len=25000)
 
 # Load neural network module
 model.load_state_dict(torch.load(model_path))
@@ -94,79 +95,89 @@ else:
 
 input_str = ''
 
-NUM_TESTS = 100
-BATCH_SIZE = 10
-total_MAE = 0.
-for _ in range(NUM_TESTS):
+if CALC_MAE:
+    NUM_TESTS = 100
+    BATCH_SIZE = 10
+    total_MAE = 0.
+    for _ in range(NUM_TESTS):
 
-    batch_X = []
-    batch_y = []
-    batch_actions = []
+        batch_X = []
+        batch_y = []
+        batch_actions = []
 
-    for _ in range(BATCH_SIZE):
-        episode_ok = False
-        while not episode_ok:
-            episode_idx = np.random.choice(np.arange(len(test_X)))
-            if len(test_X[episode_idx]) > 6:
-                episode_ok = True
+        for _ in range(BATCH_SIZE):
+            episode_ok = False
+            while not episode_ok:
+                episode_idx = np.random.choice(np.arange(len(test_X)))
+                if len(test_X[episode_idx]) > 6:
+                    episode_ok = True
 
-        step_idx = np.random.choice(np.arange(len(test_X[episode_idx]) - 5))
+            step_idx = np.random.choice(np.arange(len(test_X[episode_idx]) - 5))
 
-        batch_X.append(test_X[episode_idx][step_idx])
-        batch_y.append(test_v[episode_idx][step_idx+5])
-        batch_actions.append(test_a[episode_idx][step_idx+1:step_idx+6])
+            batch_X.append(test_X[episode_idx][step_idx])
+            batch_y.append(test_v[episode_idx][step_idx+5])
+            batch_actions.append(test_a[episode_idx][step_idx+1:step_idx+6])
 
-    batch_img = torch.from_numpy(np.array(batch_X)).to(device)
-    batch_embedding = torch.reshape(model.encoder(batch_img), [batch_img.shape[0], Z_DIM])
-    batch_actions = np.reshape(np.array(batch_actions), [len(batch_actions), 5])
-    batch_y = np.array(batch_y)
+        batch_img = torch.from_numpy(np.array(batch_X)).to(device)
+        batch_embedding = torch.reshape(model.encoder(batch_img), [batch_img.shape[0], Z_DIM])
+        batch_actions = np.reshape(np.array(batch_actions), [len(batch_actions), 5])
+        batch_y = np.array(batch_y)
 
-    # TODO: get_memory doesn't work in batches, need to fix this. In part because get_memory doesn't
-    #  return batches, but also because different actions in the batch imply interrogating different dictionaries...
-    tmp_embedding = batch_embedding
-    for i in range(batch_actions.shape[0]):
-        for a in batch_actions[i]:
-            tmp_embedding[i], _ = model.memory[a].get_memory(tmp_embedding[i])
+        # TODO: get_memory doesn't work in batches, need to fix this. In part because get_memory doesn't
+        #  return batches, but also because different actions in the batch imply interrogating different dictionaries...
+        tmp_embedding = batch_embedding
+        for i in range(batch_actions.shape[0]):
+            for a in batch_actions[i]:
+                tmp_embedding[i], _ = model.memory[a].get_memory(tmp_embedding[i])
 
-    pred_values = model.valuePredictor(tmp_embedding)
-    MAE = np.mean(np.abs(batch_y - pred_values.cpu().data.numpy()))
-    print("MAE = ", MAE)
+        pred_values = model.valuePredictor(tmp_embedding)
+        MAE = np.mean(np.abs(batch_y - pred_values.cpu().data.numpy()))
+        print("MAE = ", MAE)
 
-    total_MAE += MAE
+        total_MAE += MAE
 
-total_MAE /= float(NUM_TESTS)
-print("Total MAE: ", total_MAE)
+    total_MAE /= float(NUM_TESTS)
+    print("Total MAE: ", total_MAE)
 
 with torch.no_grad():
+    # select a random time step in a random episode. Display its current estimated value, as well as the image.
+    episode_idx = np.random.choice(np.arange(len(test_X)))
+    step_idx = np.random.choice(np.arange(len(test_X[episode_idx]) - 5))
+
     while input_str != 'q':
-        # select a random time step in a random episode. Display its current estimated value, as well as the image.
-        episode_idx = np.random.choice(np.arange(len(test_X)))
-        if len(test_X[episode_idx]) > 6:
-            step_idx = np.random.choice(np.arange(len(test_X[episode_idx])-5))
 
-            img = torch.unsqueeze(torch.from_numpy(test_X[episode_idx][step_idx]).to(device), axis=0)
-            img_embedding = torch.reshape(model.encoder(img), [img.shape[0], Z_DIM])
-            pred_value = model.valuePredictor(img_embedding)
-            print("==> Estimated current value: ", pred_value.cpu().data.numpy())
+        img = torch.unsqueeze(torch.from_numpy(test_X[episode_idx][step_idx]).to(device), axis=0)
+        img_embedding = torch.reshape(model.encoder(img), [img.shape[0], Z_DIM])
+        pred_value = model.valuePredictor(img_embedding)
+        print("==> Estimated current value: ", pred_value.cpu().data.numpy())
 
-            plt_img = np.reshape(test_X[episode_idx][step_idx], [W, H, NC])
-            plt.imshow(plt_img)
-            plt.title("Estimated value: %.4f" % (pred_value.cpu().data.numpy()))
-            plt.show()
+        plt_img = np.reshape(test_X[episode_idx][step_idx], [W, H, NC])
+        plt.imshow(plt_img)
+        plt.title("Estimated value: %.4f" % (pred_value.cpu().data.numpy()))
+        plt.show()
 
-            # input a sequence of 5 actions from the user
-            input_str = input("Enter action sequence: ")
+        # input a sequence of 5 actions from the user
+        input_str = input("Enter action sequence: ")
 
-            action_sequence = list(map(int, input_str.split(',')))
-            print("Captured action sequence: ", action_sequence)
+        action_sequence = list(map(int, input_str.split(',')))
+        print("Captured action sequence: ", action_sequence)
 
-            # call the transition model 5 times to predict the impact of the action sequence, then call the predictor module
-            #  to estimate the predicted action sequence value. Display.
-            for a in action_sequence:
-                img_embedding, _ = model.memory[a].get_memory(img_embedding)
+        # call the transition model 5 times to predict the impact of the action sequence, then call the predictor module
+        #  to estimate the predicted action sequence value. Display.
+        uncertainty = 0.
+        uncertainties = []
+        for a in action_sequence:
+            img_embedding, similarity = model.memory[a].get_memory(img_embedding)
+            uncertainty -= similarity.cpu().data.numpy()
+            uncertainties.append(-similarity.cpu().data.numpy())
 
-            pred_5step_value = model.valuePredictor(img_embedding)
-            print("==> Estimated 5-step ahead value: %.4f" % (pred_5step_value.cpu().data.numpy()))
-
-            print("================================================================================")
+        pred_5step_value = model.valuePredictor(img_embedding)
+        conviction = pred_5step_value * np.exp(uncertainty)
+        print("==> Estimated 5-step ahead value: %.4f (uncertainty: %.4f, conviction: %.4f)" % (
+              pred_5step_value.cpu().data.numpy(),
+              uncertainty,
+              conviction
+              ))
+        print("Uncertainties: ", uncertainties)
+        print("================================================================================")
 
